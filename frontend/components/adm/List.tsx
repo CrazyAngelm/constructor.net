4eab127f3b2da33405b7bdb44c2b07a87a2d955e
Александр Бахаревский <bakharevskiya@mail.ru>
2022-06-07 14:37:36 +0300

    fix(frontend): правка отображения таблицы в List

--- frontend/components/adm/List.tsx
+++ frontend/components/adm/List.tsx
@@ -10,29 +10,32 @@ interface Props {
 	rows?: Row[]
 	length: number
 	callback?: (id: number) => void
+	maxWidth?: number
 }
 
-const List = ({ rows, callback, length = 0 }: Props) => {
+const List = ({ rows, callback, length = 0, maxWidth = 20 }: Props) => {
+
+	const styleMaxWidth = {
+		maxWidth: maxWidth / (rows ? rows.length : 1) + 'vw'
+	}
 
 	return (
 		<article className={styles.list}>
 			<header>Задания</header>
-			<table className={styles.table}>
-				<thead>
-					<tr>
-						{rows?.map((p, i) => <th key={i}>{p.header}</th>)}
-					</tr>
-				</thead>
-			</table>
 			<section className={styles.content}>
 				<table className={styles.table}>
+					<thead>
+						<tr>
+							{rows?.map((p, i) => <th key={i}><div>{p.header}</div></th>)}
+						</tr>
+					</thead>
 					<tbody>
 						{Array.from({ length }).map((u, i) =>
 							<tr onClick={() => callback && callback(i)} key={i}>{
 								rows?.map((p, pi) =>
 									p.key
-										? <th key={pi}>{p.value(i)}</th>
-										: <td key={pi}>{p.value(i)}</td>
+										? <th key={pi}><div style={styleMaxWidth}>{i}</div></th>
+										: <td key={pi}><div style={styleMaxWidth} >{p.value(i)}</div></td>
 								)
 							}</tr>
 						)}
