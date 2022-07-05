9cca6fe809637d302ee9b9d60e43e31a4fb79814
Regar <bakharevskiya@mail.ru>
2022-07-05 20:17:58 +0300

    fix(frontend): changeHandler только для строк

--- frontend/lib/changeHandler.ts
+++ frontend/lib/changeHandler.ts
@@ -1,13 +1,11 @@
 import { Dispatch, SetStateAction } from 'react'
 
-export const changeHanderDtoString = <T, V>(field: keyof T, setter: Dispatch<SetStateAction<T | undefined>>)
-	: ((value: string | boolean | number) => void) => {
+export const changeHanderDtoString = <T>(field: keyof T, setter: Dispatch<SetStateAction<T | undefined>>)
+	: ((value: string) => void) => {
 	return v => {
-
 		setter(data => {
 			if (!data) return data
 			const obj: any = data
-			if (typeof obj[ field ] !== (typeof v)) throw Error('Типы не совпадают')
 			obj[ field ] = v
 			return { ...obj }
 		})
