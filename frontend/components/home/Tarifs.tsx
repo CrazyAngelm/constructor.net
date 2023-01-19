43fe970e98a1c3731fe462b4eea1e36307f387a0
Rogladon <bakharevskiya@mail.ru>
2023-01-19 17:26:50 +0300

    fix(frontend): fixes

--- frontend/components/home/Tarifs.tsx
+++ frontend/components/home/Tarifs.tsx
@@ -4,7 +4,7 @@ import f2 from '@/assets/home/second.png'
 import f3 from '@/assets/home/third.png'
 import styles from '@/styles/home/Tarifs.module.scss'
 import { useFetchData } from "@/lib/hooks/useFetchData"
-import { getLicenses, subscribe } from "@/lib/requests/subscription"
+import { freeSubscribe, getLicenses, subscribe } from "@/lib/requests/subscription"
 import { useState } from "react"
 import { useSession } from "@/lib/session/hooks"
 import { makeFetcher } from "@/lib/fetchers"
@@ -13,25 +13,47 @@ import { YooCheckoutWidget } from "@/lib/YooCheckoutWidget"
 import Auth from "../auth/Auth"
 import Router from "next/router"
 import Order from "../pricing/Order"
+import { ApiError } from "@/lib/requests"
+import Modal from "../controls/Modal"
 
 
 const Tarifs = () => {
-	const { data, update, error } = useFetchData({}, getLicenses)
-
 	const [ errorAuth, setErrorAuth ] = useState<boolean>(false);
+	const [ error, setError ] = useState<string>()
 	const [ visible, setVisible ] = useState(false)
 
+	const { data, update } = useFetchData({}, getLicenses)
 	const session = useSession()
 
 	const onSubmit = async () => {
-
 		if (session == null || session == 'loading') return
 
-		Router.push('./subscribe-sucessful')
+		try {
+			const res = await makeFetcher(freeSubscribe)({
+				userId: session.user?.id,
+				licenseId: 1
+			})
+			Router.push('/subscribe-sucessful')
+		} catch (err) {
+			if (err instanceof ApiError) {
+				if (err.status == 412) setError('Вы уже имеете активную подписку, чтобы оформить новую отмените текущую в личном кабинете')
+				else setError(err.message)
+			}
+			console.log("error", err)
+		} finally {
+			setVisible(false)
+		}
 	}
 
 	return (
 		<article className={styles.tarifs} id="pricing">
+			<Modal closeCallback={() => setError(undefined)}
+				visible={error != undefined}>
+				<section className={styles.error}>
+					<header>Ошибка!</header>
+					<div>{error}</div>
+				</section>
+			</Modal>
 			{visible && <Order okCallback={onSubmit}
 				closeCallback={() => setVisible(false)}
 				license={data ? data[ 0 ] : undefined} />}
