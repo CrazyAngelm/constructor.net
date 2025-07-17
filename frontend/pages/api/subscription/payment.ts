669bafda7f3cd80e004753a7ddf5790563a03162
Rogladon <bakharevskiya@mail.ru>
2025-07-17 13:42:20 +0300

    fix

--- frontend/pages/api/subscription/payment.ts
+++ frontend/pages/api/subscription/payment.ts
@@ -4,6 +4,7 @@ import { Prisma, PrismaClient, Subscription } from '@prisma/client'
 import { checkout } from '@/lib/yookassa/checkout'
 import { getDefaultHandler } from '@/lib/api/apiHandler'
 import { response } from '@/lib/api/response'
+import { Payment } from '@a2seven/yoo-checkout'
 
 export const config = { api: { bodyParser: false } }
 
@@ -15,68 +16,61 @@ const addMonth = (d: Date) => new Date(d.setMonth(d.getMonth() + 1))
 
 /** Payload YooKassa */
 interface NotificationPayment {
+	type: string
 	event: string
-	object: {
-		id: string
-		status: 'waiting_for_capture' | 'succeeded' | 'canceled' | string
-		amount: { value: string }
-		payment_method: { id: string; title: string }
-	}
+	object: Payment
 }
 
 handler.post(response(async (req, res) => {
+	console.log(req.body)
+	const n = req.body as NotificationPayment
 
-	/* 1. читаем «сырое» тело → сразу отдаём 200 */
-		/* const sig = (req.headers['x-content-hmac'] as string) ?? ''
-		if (!checkout.verifyWebhookSignature(raw.toString(), sig)) return */
-		console.log(req.body)
-		const n = JSON.parse(req.body) as NotificationPayment
-		console.log(n)
-		if (!n.event.startsWith('payment')) return {}
+	console.log(n)
+	if (!n.event.startsWith('payment')) return {}
 
-		const p = n.object
-		const dbPay = await prisma.payment.findUnique({ where: { id: p.id } })
-		console.log(dbPay)
-		if (!dbPay || dbPay.confirmed) return {}                    // дубликат/неизвестный
+	const p = n.object
+	const dbPay = await prisma.payment.findUnique({ where: { id: p.id } })
+	console.log(dbPay)
+	if (!dbPay || dbPay.confirmed) return {}                    // дубликат/неизвестный
 
-		const isBindCard = p.amount.value === '1.00'
+	const isBindCard = p.amount.value === '1.00'
 
-		try {
-			await prisma.$transaction(async (tx) => {
-				switch (p.status) {
-					case 'waiting_for_capture':
-						await checkout.capturePayment(p.id) // авторизация → capture
-						return
+	try {
+		await prisma.$transaction(async (tx) => {
+			switch (p.status) {
+				case 'waiting_for_capture':
+					await checkout.capturePayment(p.id) // авторизация → capture
+					return
 
-					case 'succeeded':
-						if (isBindCard) {
-							/* обновляем токен оплаты во всех активных подписках */
-							await tx.subscription.updateMany({
-								where: { userId: dbPay.userId, canceled: false },
-								data: {
-									paymentToken: p.payment_method.id,
-									paymentTitle: p.payment_method.title
-								}
-							})
-						} else {
-							await handleSucceeded(tx, dbPay, p)
-						}
-						await tx.payment.update({ where: { id: dbPay.id }, data: { confirmed: true } })
-						return
-
-					case 'canceled':
+				case 'succeeded':
+					if (isBindCard) {
+						/* обновляем токен оплаты во всех активных подписках */
 						await tx.subscription.updateMany({
-							where: { userId: dbPay.userId, licenseId: dbPay.licenseId, canceled: false },
-							data: { active: false }
+							where: { userId: dbPay.userId, canceled: false },
+							data: {
+								paymentToken: p.payment_method.id,
+								paymentTitle: p.payment_method.title
+							}
 						})
-						return
-				}
-				return
-			})
-		} catch (err) {
-			console.error('Webhook processing error', err)
-		}
-		return {}
+					} else {
+						await handleSucceeded(tx, dbPay, p)
+					}
+					await tx.payment.update({ where: { id: dbPay.id }, data: { confirmed: true } })
+					return
+
+				case 'canceled':
+					await tx.subscription.updateMany({
+						where: { userId: dbPay.userId, licenseId: dbPay.licenseId, canceled: false },
+						data: { active: false }
+					})
+					return
+			}
+			return
+		})
+	} catch (err) {
+		console.error('Webhook processing error', err)
+	}
+	return {}
 }))
 
 /** «Оплачено» — создать/продлить подписку */
